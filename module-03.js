9414ffe18f9938131c5c5d62cb9dbd00dc9419fa
Alexandr Volokh <65076117+angelus72008@users.noreply.github.com>
2020-09-09 14:53:54 +0300

    first commit

---
+++ module-03.js
@@ -0,0 +1,72 @@
+// const testObj = {
+//     test: '123',
+// };
+// testObj.test = 'test'
+// // delete testObj.test;
+// console.log(testObj['test']);
+
+// const obj = {
+//     defaultValue: 0,
+//     add: function() {
+//     obj.defaultValue += 1;
+//     const a = 10;
+//     return a;
+//     }
+// }
+// obj.add();
+// console.log(obj);
+
+// const obj = {
+//     defaultValue: 0,
+//     add: function(n) {
+//        return obj.defaultValue += n;
+//     },
+//     minus: function(n) {
+//        return obj.defaultValue -= n;
+//     },
+//     myltiply: function(n) {
+//        return obj.defaultValue *= n;
+//     },
+//     split: function(n) {
+//        return obj.defaultValue /= n;
+//     },
+// }
+
+// // console.log(obj);
+// console.log(obj.add(2));
+
+
+// const obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
+// let total = 0;
+// for (let key in obj) {
+//     console.log( `${key} is equal of ${obj[key]}`);
+//     total+=obj[key];
+// }
+
+// console.log(total);
+
+// console.log(Object.entries(obj));
+
+// let obj1 = {
+//   a: 1,
+//   b: 2,
+// }
+
+// let obj2 = {
+//     a: 5,
+//     b: 7
+// }
+
+
+// let q = 15;
+// let p = 20;
+
+// let obj3 = {q, ...obj1, p, ...obj2};
+
+// console.log(obj3);
+
+ 
